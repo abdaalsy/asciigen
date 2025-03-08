75c7122e4a2eb804a4d6bc87df7f59dac6271caa
Abdaal <abdaalsy@gmail.com>
2025-03-08 14:58:15 -0500

    temporary host on render domain

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const ROOT = "https://asciigen.xyz";
+const ROOT = "https://asciiweb.onrender.com";
 const fileInputRadio = document.getElementById("file-input");
 const urlInputRadio = document.getElementById("url-input");
 const mainInput = document.getElementById("input");
